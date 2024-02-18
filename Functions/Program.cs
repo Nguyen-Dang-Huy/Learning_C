b138532aa34838f6a8287dbafea61acb3bb9a7b6
Nguyen Dang Huy <huy.nguyen2311174@hcmut.edu.vn>
2024-02-18 16:22:16 +0700

    Update Folder Functions

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -20,10 +20,28 @@ namespace Math_Operations
             }
             Console.Write(gt);
         }
+        static void TangGiaTri(ref int n)
+        {
+            n = n + 1;
+        }
+        static void GiamGiaTri(out int b)
+        {
+            b=0;
+        }
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            TinhGiaiThua();
+            //TinhGiaiThua();
+
+            // Tham chiều: khiến giá trị đi vào hàm sẽ thay đổi giá trị bên ngoài hàm
+            // Tham chiếu dùng từ khóa out hoặc ref
+            // out bắt buộc người dùng phải gán giá trị cho biến trước khi thoát khỏi hàm
+            // ref bắt buộc người dùng phải khởi tạo giá trị cho biến trước khi vào hàm
+            int a = 10,b=10;
+            TangGiaTri(ref a); // Lúc này giá trị của a=b=11
+            Console.WriteLine("a= {0}, b= {1}", a,b);
+            GiamGiaTri(out b);
+            Console.WriteLine("a= {0}, b= {1}", a, b);
             Console.ReadLine();
         }
     }
